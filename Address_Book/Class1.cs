e321fdfa0e71c3ef9bd200a3086afc9183062a2e
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-13 22:06:53 +0530

    Update Contact using Name

---
+++ Address_Book/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Address_Book
+{
+    class AdddressBookBuilder
+    {
+        public List<Data> list;
+        public AdddressBookBuilder()
+        {
+            this.list = new List<Data>();
+        }
+        public void AddContact(Data dataobj)
+        {
+            this.list.Add(dataobj);
+        }
+        public void Display_Contacts()
+        {
+            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+            Console.WriteLine("--------------Contacts in Address Books System--------------");
+
+            foreach (var i in this.list)
+            {
+                Console.WriteLine($"{i.First_Name} : {i.Last_Name} : {i.Address} : {i.City} : {i.State} : {i.Zip} : {i.Phone_No} : {i.Email}");
+            }
+
+        }
+        public int Update_Contact(string FirstName)
+        {
+            int index = list.FindIndex(data => data.First_Name.Equals(FirstName));
+            return index;
+
+        }
+    }
+}
---
+++ Address_Book/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Address_Book
+{
+	public class Data
+	{
+        public String First_Name { get; set; }
+        public String Last_Name { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public int Zip { get; set; }
+        public long Phone_No { get; set; }
+        public String Email { get; set; }
+    }
+}
